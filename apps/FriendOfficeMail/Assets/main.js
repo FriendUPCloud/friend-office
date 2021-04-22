d81b8db41d946b98d83068b6336228b024ecf8db
Hogne Titlestad <ht@friendup.cloud>
2021-04-22 22:10:54 +0200

    Login procedure.

--- apps/FriendOfficeMail/Assets/main.js
+++ apps/FriendOfficeMail/Assets/main.js
@@ -12,18 +12,47 @@
 
 Application.run = function( msg )
 {
-	
+	ge( 'MainFrame' ).style.opacity = 0;
 }
 
 function initMainFrame()
 {
-	ge( 'MainFrame' ).contentWindow.postMessage( { 'command': 'register_friend' }, '*' );
+	//ge( 'MainFrame' ).contentWindow.postMessage( { 'command': 'register_friend' }, '*' );
 }
 
 window.addEventListener( 'message', function( msg )
 {
-	console.log( 'Friend received a message!', msg );
+	if( msg && !msg.data ) return;
+	let message = msg.data;
+	if( !message.command ) return;
+	
+	if( message.command == 'login_with_friend' )
+	{
+		let d = ge( 'Login' );
+		if( !d ) d = document.createElement( 'div' );
+		d.id = 'Login';
+		let f = new File( 'Progdir:Assets/login.html' );
+		f.onLoad = function( data )
+		{
+			d.innerHTML = data;
+			document.body.appendChild( d );
+		}
+		f.load();
+	}
+	else if( message.command == 'register_with_friend' )
+	{
+		ge( 'MainFrame' ).style.opacity = 1;
+	}
 } );
+
+function executeLogin()
+{
+	ge( 'MainFrame' ).contentWindow.postMessage( {
+		command: 'login',
+		username: ge( 'loginUser' ).value,
+		password: ge( 'loginPass' ).value
+	}, '*' );
+}
 
 
 
