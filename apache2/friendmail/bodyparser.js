fe4425162f83e8f08152e9632f245a1f1745a95a
Hogne Titlestad <ht@friendup.cloud>
2021-04-28 14:05:46 +0200

    Some logging.

--- apache2/friendmail/bodyparser.js
+++ apache2/friendmail/bodyparser.js
@@ -1,4 +1,4 @@
-<script>
+<script type="text/javascript">
 	// Fix popup links
 	function linkFixer()
 	{
@@ -9,6 +9,7 @@
 				a[b].setAttribute( 'target', '' );
 		}
 	}
+	// TODO: Make an event listener that makes better sense
 	window.addEventListener( 'click', function( msg )
 	{
 		// Just fix popup links!
@@ -17,35 +18,39 @@
 	} );
 	window.addEventListener( 'message', function( msg )
 	{
-		console.log( 'Hello thingie!', msg );
-		if( msg && msg.data && msg.data.command && msg.data.command == 'register_friend' )
-		{
-			console.log( 'Test' );
-			//console.log( 'We got it!', msg.source );
-			//msg.source.postMessage( { command: 'response', response: 'received!' }, '*' );
-		}
-		if( msg && msg.data && msg.data.command && msg.data.command == 'login' )
-		{
-			if( !document.getElementById( 'login' ) )
-				return;
-			document.getElementById( 'login' ).value = msg.data.username;
-			document.getElementById( 'pwd' ).value = msg.data.password;
-			Authorize.Submit();
-		}
+		if( !msg || !msg.data || !msg.data.command ) return;
 		
+		let mes = msg.data;
+		let cmd = mes.command;
+		
+		switch( cmd )
+		{
+			case 'register_friend':
+				break;
+			case 'login':
+			{
+				if( !document.getElementById( 'login' ) )
+				return;
+				document.getElementById( 'login' ).value = msg.data.username;
+				document.getElementById( 'pwd' ).value = msg.data.password;
+				Authorize.Submit();
+				break;
+			}
+		}
 	} );
 
 	// Don't yell on unloading!
-	window.onbeforeunload = function () {
-	}
+	window.onbeforeunload = function (){}
 
 	// Call Friend!
 	if( window.parent )
 	{
+		// We need to log in
 		if( document.getElementById( 'login' ) )
 		{
 			window.parent.postMessage( { command: 'login_with_friend' }, '*' );
 		}
+		// We are logged in
 		else
 		{
 			window.parent.postMessage( { command: 'register_with_friend' }, '*' );
@@ -54,7 +59,4 @@
 
 	// Fix links now!
 	linkFixer();
-	console.log( 'Hello' );
-
-
 </script>
