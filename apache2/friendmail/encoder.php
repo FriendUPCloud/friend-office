9e1853d4b20daf5443f119e8d1399313c77d720d
Hogne Titlestad <ht@friendup.cloud>
2021-06-11 10:35:57 +0200

    Do it.

--- apache2/friendmail/encoder.php
+++ apache2/friendmail/encoder.php
@@ -3,6 +3,6 @@
 $html = file_get_contents('php://stdin');
 
 $html = str_ireplace('</head>', file_get_contents( __DIR__ . '/theme.css' ) . '</head>', $html);
-$html = str_ireplace('</body>', file_get_contents( __DIR__ . '/bodyparser.js' ) . '</body>', $html);
+$html = str_ireplace('</head>', file_get_contents( __DIR__ . '/bodyparser.js' ) . '</head>', $html);
 
 file_put_contents('php://stdout', $html);
