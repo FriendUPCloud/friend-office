004f49a51a492333e9c359db969a634b79405756
Hogne Titlestad <ht@friendup.cloud>
2021-06-29 14:11:48 +0200

    Friend mail output.

--- apps/FriendOfficeMail/Module/module.php
+++ apps/FriendOfficeMail/Module/module.php
@@ -20,7 +20,7 @@ if( trim( $User->Email ) && trim( $User->FullName ) )
 	$Mail = new Mailer();
 
 	$Mail->setSubject( 'User requests Friend Mail account' );
-	$Mail->setContent( '<table align="center" width="600" cellspacing="0" cellpadding="15" bgcolor="#f0f0f0"><tr><td bgcolor="#ccddee">FRIEND SKY</td></tr><tr><td bgcolor="#f8f8f8"><h2>New registration for Friend Mail</h2><p>The user: ' . $User->FullName . ' is requesting a Friend Mail account on Friend Sky. His e-mail is:<br><br><ul><li>' . $User->Email . '</li></ul><br><br>Thank you!</p></td></tr></table>' . print_r( $User, 1 ) );
+	$Mail->setContent( '<table align="center" width="600" cellspacing="0" cellpadding="15" bgcolor="#f0f0f0"><tr><td bgcolor="#ccddee">FRIEND SKY</td></tr><tr><td bgcolor="#f8f8f8"><h2>New registration for Friend Mail</h2><p>The user: ' . $User->FullName . ' is requesting a Friend Mail account on Friend Sky. His e-mail is:<br><br><ul><li>' . $User->Email . '</li></ul><br><br>Thank you!</p></td></tr></table>' );
 	$Mail->setFrom( 'info@friendos.com' );
 	$Mail->addRecipient( 'hogne.friendup@gmail.com' );
 	$Mail->addRecipient( 'info@friendos.com' );
